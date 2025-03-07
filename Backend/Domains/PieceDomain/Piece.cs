704ba2b77a421195d74299a9aa03b025eb192a80
Thomas Lind <thli49474@edu.ucl.dk>
2025-03-07 12:37:04 +0100

    Added piece, pieceservice and Interface for pieceservice

---
+++ Backend/Domains/PieceDomain/Piece.cs
@@ -0,0 +1,23 @@
+﻿using Common.DTOs;
+using Common.Enums;
+
+namespace Backend.Domains.PieceDomain
+{
+	public class Piece
+	{
+		public Piece(int id, ColourEnum colour)
+		{
+			ID = id;
+			Colour = colour;
+			PosIndex = null;
+			IsInPlay = false;
+			IsFinished = false;
+		}
+
+		int ID { get; }
+		ColourEnum Colour { get; }
+		PosIndex? PosIndex { get; set; }
+		bool IsInPlay { get; set; }
+		bool IsFinished { get; set; }
+	}
+}
