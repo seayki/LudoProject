0131c3bf2c92e7593c013d67b89d9f7e63314315
Mathias <mmth70675@edu.ucl.dk>
2025-03-07 12:08:34 +0100

    Test

--- UnitTests/PlayerTests/PlayerDomainTest.cs
+++ UnitTests/PlayerTests/PlayerDomainTest.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace UnitTests.PlayerTests
 {
     public class PlayerDomainTest
     {
         [Fact]
-        public void test
+        public void SelectPiece_ReturnTrue()
         {
+            int pieceId = 1;
+            bool result = 
+
+            Assert.True(result);
 
         }
     }
