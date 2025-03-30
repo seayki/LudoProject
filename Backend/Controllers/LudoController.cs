9e0bb16aa79cc461ce11ccee4038ac14acfc8182
Thomas Lind <thli49474@edu.ucl.dk>
2025-03-30 22:01:31 +0200

    Added unimplemented ludo controller

---
+++ Backend/Controllers/LudoController.cs
@@ -0,0 +1,27 @@
+﻿using Backend.Domains.PieceDomain;
+using Common.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class LudoController : ControllerBase
+	{
+		public async Task<IActionResult> FindValidMoves(int diceroll)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<IActionResult> MoveSelectedPiece(Guid pieceID, PosIndex posIndex)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<IActionResult> StartGame(int PlayerNumber, int BoardSize)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
