86268b5cfea6719d0b11b1df48d7b3d2d179ff5f
isak <isak@>
2025-04-21 10:48:32 +0200

    Services Registered

--- Backend/Services/GameManagerService/IGameManagerService.cs
+++ Backend/Services/GameManagerService/IGameManagerService.cs
@@ -1,7 +1,17 @@
-﻿namespace Backend.Services.GameManagerService
+﻿using Backend.Domains.BoardDomain;
+using Backend.Domains.PieceDomain;
+using Backend.Domains.PlayerDomain;
+
+namespace Backend.Services.GameManagerService
 {
     public interface IGameManagerService
     {
-
+        (Board Board, List<Player> Players) CreateNewGame(int playerCount, int boardSize, int lengthOfColourZone);
+        List<Player> RollForPlayerOrder();
+        void Roll(int roll);
+        List<Piece>? GetMovablePieces();
+        List<Piece> MovePiece(Guid pieceId);
+        bool CanRollAgain();
+        Guid NextTurn();
     }
 }
